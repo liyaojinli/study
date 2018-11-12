b1ddb9b068efef19da1bf5ef531bdada15daf96f
liyaojinli <307046847@qq.com>
2018-11-12 20:45:37 +0800

    add springboot mock test demo

---
+++ spring-boot-cloud/demo_bookManager/src/test/java/org/liyaojin/springboot/demo/bookmanager/BookControllerMockTests.java
@@ -0,0 +1,34 @@
+package org.liyaojin.springboot.demo.bookmanager;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BookmanagerApplication.class)
+@WebAppConfiguration
+public class BookControllerMockTests {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    public void testGetBooksByReader() {
+
+    }
+}
